79a1f9eccf7e15c6c8e80df1c2ca5a2b3c7d6dc5
Wale Bnji <walebnji@gmail.com>
2023-06-17 22:34:07 +0100

    Configuring tailwind

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     Musica
-    </div>
-  );
+  return <div className='App'>Musica</div>;
 }
 
 export default App;
